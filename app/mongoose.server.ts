d573953b1df93ac2eec476c281e27848e4844bc4
Jacob Kwasi Bordes <kwasibordesjacob@gmail.com>
2024-04-25 10:23:36 +0000

    project setup

---
+++ app/mongoose.server.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI as string);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
+
+export default db;
